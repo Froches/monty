8bac25bc4e748cb73d7157f93bc8585eba4540f4
Froches <michaelfavour418@gmail.com>
2023-08-27 16:31:06 -0700

    push pall

--- main.c
+++ main.c
@@ -1,46 +1,71 @@
 #include "monty.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	size_t i, j;
-	unsigned int line_number;
-	char opcode[10];
-	instruction_t *instr;
-	stack_t *temp, *stack = NULL;
+	size_t j;
+	int value;
+	FILE *file;
+	char opcode[10], line[256];
+	stack_t *temp, *stack;
+	unsigned int line_number = 1;
 	instruction_t instructions[] = {
 		{"push", push},
-		{"add", add},
 		{"pall", pall}
-	};
-	char *input[] = {"push 5", "push 3", "pall"};
+	} ;
 
-	line_number = 1;
-	for (i = 0; i < sizeof(input) / sizeof(input[0]); i++)
-	{
-		scanf(input[i], "%s", opcode);
-		instr = NULL;
-		for (j = 0; j < sizeof(instructions) / sizeof(instructions[0]); j++)
-		{
-			if (strcmp(opcode, instructions[j].opcode) == 0)
-			{
-				instr = &instructions[j];
-				break;
-			}
-		}
-		if (instr == NULL)
-		{
-			fprintf(stderr, "U: %s at %u\n", opcode, line_number);
-			exit(EXIT_FAILURE);
-		}
-		instr->f(&stack, line_number);
-		line_number++;
-	}
-	printf("%d\n", stack->n);
-	while (stack != NULL)
-	{
-		temp = stack;
-		stack = stack->next;
-		free(temp);
-	}
-	return (0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error opening file: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    stack = NULL;
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        line[strcspn(line, "\n")] = '\0'; /* Remove newline character */
+
+        if (sscanf(line, "%s %d", opcode, &value) == 2 && strcmp(opcode, "push") == 0)
+        {
+            push(&stack, value);
+        }
+        else
+        {
+            for (j = 0; j < sizeof(instructions) / sizeof(instructions[0]); j++)
+            {
+                if (strcmp(opcode, instructions[j].opcode) == 0)
+                {
+                    instructions[j].f(&stack, line_number);
+                    break;
+                }
+            }
+
+            if (j == sizeof(instructions) / sizeof(instructions[0]))
+            {
+                fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+                fclose(file);
+                return (EXIT_FAILURE);
+            }
+        }
+
+        line_number++;
+    }
+
+    fclose(file);
+
+    /* Clean up: free the remaining stack nodes */
+    while (stack != NULL)
+    {
+        temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
+    return 0;
 }
