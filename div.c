8baad4d678cb8848a7346752df8c04a64141159d
Froches <michaelfavour418@gmail.com>
2023-08-28 02:53:05 -0700

    sub, div, mul, mod.

--- div.c
+++ div.c
@@ -3,9 +3,16 @@
 void divi(stack_t **stack, unsigned int line_number)
 {
 	int quotient;
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
